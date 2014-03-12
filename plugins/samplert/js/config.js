02637f5fbf8bca13f5abcd4fc4ede58ea8bee484
Matthew Smith <matt@matthewsmith.com>
2014-03-12 17:53:03 +0000

    Update plugin variable error #14

--- plugins/samplert/js/config.js
+++ plugins/samplert/js/config.js
@@ -16,8 +16,8 @@ plugin.samplert = {
 		plugin.samplert.pageUniqId = pageUniqId;
 		plugin.samplert.pluginUniqId = pluginUniqId;
 
-		$('#samplert-var1').val($('#'+plugin.samplert.pluginUniqId).attr('data-var1'));
-		$('#samplert-var2').val($('#'+plugin.samplert.pluginUniqId).attr('data-var2'));
+		$('#samplert-var1').val($('#'+plugin.samplert.pluginUniqId).data('var1'));
+		$('#samplert-var2').val($('#'+plugin.samplert.pluginUniqId).data('var2'));
 
 	},
 
@@ -25,8 +25,8 @@ plugin.samplert = {
 	update:function(el){
 		
 		// an easy way to pass data to your plugin is to set data-[var] attributes
-		$('#'+plugin.samplert.pluginUniqId).attr('data-var1', $('#samplert-var1').val());
-		$('#'+plugin.samplert.pluginUniqId).attr('data-var2', $('#samplert-var2').val());
+		$('#'+plugin.samplert.pluginUniqId).data('var1', $('#samplert-var1').val());
+		$('#'+plugin.samplert.pluginUniqId).data('var2', $('#samplert-var2').val());
 
 		// show a success message when you are done
 		message.showMessage('success', 'Plugin updated successfully');
