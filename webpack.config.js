49e40635959a56fbbcb2dcc7bc6fa626875bd340
Dominik Pieper <github@pieperhome.de>
2016-11-24 15:23:54 +0100

    Remove the testing part

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,7 @@
-/**
- * @author: @AngularClass
- */
-
-// Look in ./config folder for webpack.dev.js
 switch (process.env.NODE_ENV) {
   case 'prod':
   case 'production':
     module.exports = require('./public/config/webpack.prod');
-    break;
-  case 'test':
-  case 'testing':
-    module.exports = require('./public/config/webpack.test');
     break;
   case 'dev':
   case 'development':
