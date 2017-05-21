6a4d3dd8d24866ed01242d91b5e0a282e3d04ec8
madoublet <matt@matthewsmith.com>
2017-05-20 22:13:18 -0400

    update reCAPTCHA plugin to support enable/disable button

--- public/themes/bootstrap/js/site.all.js
+++ public/themes/bootstrap/js/site.all.js
@@ -52,6 +52,7 @@ PR_NOCODE:"nocode",PR_PLAIN:"pln",PR_PUNCTUATION:"pun",PR_SOURCE:"src",PR_STRING
  * @license MIT (https://github.com/pixelcog/parallax.js/blob/master/LICENSE)
  */
 !function(t,i,e,s){function o(i,e){var h=this;"object"==typeof e&&(delete e.refresh,delete e.render,t.extend(this,e)),this.$element=t(i),!this.imageSrc&&this.$element.is("img")&&(this.imageSrc=this.$element.attr("src"));var r=(this.position+"").toLowerCase().match(/\S+/g)||[];return r.length<1&&r.push("center"),1==r.length&&r.push(r[0]),("top"==r[0]||"bottom"==r[0]||"left"==r[1]||"right"==r[1])&&(r=[r[1],r[0]]),this.positionX!=s&&(r[0]=this.positionX.toLowerCase()),this.positionY!=s&&(r[1]=this.positionY.toLowerCase()),h.positionX=r[0],h.positionY=r[1],"left"!=this.positionX&&"right"!=this.positionX&&(this.positionX=isNaN(parseInt(this.positionX))?"center":parseInt(this.positionX)),"top"!=this.positionY&&"bottom"!=this.positionY&&(this.positionY=isNaN(parseInt(this.positionY))?"center":parseInt(this.positionY)),this.position=this.positionX+(isNaN(this.positionX)?"":"px")+" "+this.positionY+(isNaN(this.positionY)?"":"px"),navigator.userAgent.match(/(iPod|iPhone|iPad)/)?(this.iosFix&&!this.$element.is("img")&&this.$element.css({backgroundImage:"url("+this.imageSrc+")",backgroundSize:"cover",backgroundPosition:this.position}),this):navigator.userAgent.match(/(Android)/)?(this.androidFix&&!this.$element.is("img")&&this.$element.css({backgroundImage:"url("+this.imageSrc+")",backgroundSize:"cover",backgroundPosition:this.position}),this):(this.$mirror=t("<div />").prependTo("body"),this.$slider=t("<img />").prependTo(this.$mirror),this.$mirror.addClass("parallax-mirror").css({visibility:"hidden",zIndex:this.zIndex,position:"fixed",top:0,left:0,overflow:"hidden"}),this.$slider.addClass("parallax-slider").one("load",function(){h.naturalHeight&&h.naturalWidth||(h.naturalHeight=this.naturalHeight||this.height||1,h.naturalWidth=this.naturalWidth||this.width||1),h.aspectRatio=h.naturalWidth/h.naturalHeight,o.isSetup||o.setup(),o.sliders.push(h),o.isFresh=!1,o.requestRender()}),this.$slider[0].src=this.imageSrc,void((this.naturalHeight&&this.naturalWidth||this.$slider[0].complete)&&this.$slider.trigger("load")))}function h(s){return this.each(function(){var h=t(this),r="object"==typeof s&&s;this==i||this==e||h.is("body")?o.configure(r):h.data("px.parallax")||(r=t.extend({},h.data(),r),h.data("px.parallax",new o(this,r))),"string"==typeof s&&o[s]()})}!function(){for(var t=0,e=["ms","moz","webkit","o"],s=0;s<e.length&&!i.requestAnimationFrame;++s)i.requestAnimationFrame=i[e[s]+"RequestAnimationFrame"],i.cancelAnimationFrame=i[e[s]+"CancelAnimationFrame"]||i[e[s]+"CancelRequestAnimationFrame"];i.requestAnimationFrame||(i.requestAnimationFrame=function(e){var s=(new Date).getTime(),o=Math.max(0,16-(s-t)),h=i.setTimeout(function(){e(s+o)},o);return t=s+o,h}),i.cancelAnimationFrame||(i.cancelAnimationFrame=function(t){clearTimeout(t)})}(),t.extend(o.prototype,{speed:.2,bleed:0,zIndex:-100,iosFix:!0,androidFix:!0,position:"center",overScrollFix:!1,refresh:function(){this.boxWidth=this.$element.outerWidth(),this.boxHeight=this.$element.outerHeight()+2*this.bleed,this.boxOffsetTop=this.$element.offset().top-this.bleed,this.boxOffsetLeft=this.$element.offset().left,this.boxOffsetBottom=this.boxOffsetTop+this.boxHeight;var t=o.winHeight,i=o.docHeight,e=Math.min(this.boxOffsetTop,i-t),s=Math.max(this.boxOffsetTop+this.boxHeight-t,0),h=this.boxHeight+(e-s)*(1-this.speed)|0,r=(this.boxOffsetTop-e)*(1-this.speed)|0;if(h*this.aspectRatio>=this.boxWidth){this.imageWidth=h*this.aspectRatio|0,this.imageHeight=h,this.offsetBaseTop=r;var n=this.imageWidth-this.boxWidth;this.offsetLeft="left"==this.positionX?0:"right"==this.positionX?-n:isNaN(this.positionX)?-n/2|0:Math.max(this.positionX,-n)}else{this.imageWidth=this.boxWidth,this.imageHeight=this.boxWidth/this.aspectRatio|0,this.offsetLeft=0;var n=this.imageHeight-h;this.offsetBaseTop="top"==this.positionY?r:"bottom"==this.positionY?r-n:isNaN(this.positionY)?r-n/2|0:r+Math.max(this.positionY,-n)}},render:function(){var t=o.scrollTop,i=o.scrollLeft,e=this.overScrollFix?o.overScroll:0,s=t+o.winHeight;this.visibility=this.boxOffsetBottom>t&&this.boxOffsetTop<s?"visible":"hidden",this.mirrorTop=this.boxOffsetTop-t,this.mirrorLeft=this.boxOffsetLeft-i,this.offsetTop=this.offsetBaseTop-this.mirrorTop*(1-this.speed),this.$mirror.css({transform:"translate3d(0px, 0px, 0px)",visibility:this.visibility,top:this.mirrorTop-e,left:this.mirrorLeft,height:this.boxHeight,width:this.boxWidth}),this.$slider.css({transform:"translate3d(0px, 0px, 0px)",position:"absolute",top:this.offsetTop,left:this.offsetLeft,height:this.imageHeight,width:this.imageWidth,maxWidth:"none"})}}),t.extend(o,{scrollTop:0,scrollLeft:0,winHeight:0,winWidth:0,docHeight:1<<30,docWidth:1<<30,sliders:[],isReady:!1,isFresh:!1,isBusy:!1,setup:function(){if(!this.isReady){var s=t(e),h=t(i);h.on("scroll.px.parallax load.px.parallax",function(){var t=o.docHeight-o.winHeight,i=o.docWidth-o.winWidth;o.scrollTop=Math.max(0,Math.min(t,h.scrollTop())),o.scrollLeft=Math.max(0,Math.min(i,h.scrollLeft())),o.overScroll=Math.max(h.scrollTop()-t,Math.min(h.scrollTop(),0)),o.requestRender()}).on("resize.px.parallax load.px.parallax",function(){o.winHeight=h.height(),o.winWidth=h.width(),o.docHeight=s.height(),o.docWidth=s.width(),o.isFresh=!1,o.requestRender()}),this.isReady=!0}},configure:function(i){"object"==typeof i&&(delete i.refresh,delete i.render,t.extend(this.prototype,i))},refresh:function(){t.each(this.sliders,function(){this.refresh()}),this.isFresh=!0},render:function(){this.isFresh||this.refresh(),t.each(this.sliders,function(){this.render()})},requestRender:function(){var t=this;this.isBusy||(this.isBusy=!0,i.requestAnimationFrame(function(){t.render(),t.isBusy=!1}))}});var r=t.fn.parallax;t.fn.parallax=h,t.fn.parallax.Constructor=o,t.fn.parallax.noConflict=function(){return t.fn.parallax=r,this},t(e).on("ready.px.parallax.data-api",function(){t('[data-parallax="scroll"]').parallax()})}(jQuery,window,document);
+
 var respond = respond || {};
 
 /*
@@ -192,6 +193,21 @@ respond.plugins = (function() {
         cur = cur.parentNode; //go up
       }
       return cur; //will return null if not found
+    },
+
+    /**
+     * Gets the query string paramger
+     * @param {String} name
+     * @param {String} url
+     */
+    getQueryStringParam: function(name, url) {
+      if (!url) url = window.location.href;
+      name = name.replace(/[\[\]]/g, "\\$&");
+      var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+          results = regex.exec(url);
+      if (!results) return null;
+      if (!results[2]) return '';
+      return decodeURIComponent(results[2].replace(/\+/g, " "));
     },
 
     /**
@@ -359,14 +375,64 @@ respond.form = (function() {
      */
     setup: function() {
 
-      var forms, x;
+      var form, forms, x, status, id, success, error, holder, recaptchaError, siteKey, submit;
 
       // setup [respond-form]
       forms = document.querySelectorAll('[respond-form]');
 
-      // setup submit event for form
+      // setup form
       for(x=0; x<forms.length; x++) {
+
+        // add submit
         forms[x].addEventListener('submit', respond.form.submitForm);
+
+      }
+
+      // handle messaging
+      status = respond.plugins.getQueryStringParam('formstatus');
+
+      if(status != '' && status != null) {
+
+        id = respond.plugins.getQueryStringParam('formid');
+
+        if(id != '' && id != null) {
+
+          form = document.querySelector('#' + id);
+
+          if(form != null) {
+            success = form.getAttribute('data-success') || '';
+            error = form.getAttribute('data-error') || '';
+            recaptchaError = form.getAttribute('data-recaptcha-error') || '';
+
+            // show success
+            if(status == 'success' && success != '') {
+              respond.toast.show('success', success);
+            }
+            else if(status == 'success'){
+              respond.toast.show('success');
+            }
+
+            // show error
+            if(status == 'error' && error != '') {
+              respond.toast.show('failure', error);
+            }
+            else if(status == 'error') {
+              respond.toast.show('failure');
+            }
+
+            // show error
+            if(status == 'recaptcha-failure' && recaptchaError != '') {
+              respond.toast.show('failure', recaptchaError);
+            }
+            else if(status == 'recaptcha-failure') {
+              respond.toast.show('failure');
+            }
+
+
+          }
+
+        }
+
       }
 
     },
@@ -501,6 +567,60 @@ respond.form = (function() {
   		for(x=0; x<els.length; x++){
   			els[x].selectedIndex = 0;
   		}
+
+  	},
+
+    /**
+     * Setup recaptcha
+     */
+  	setupRecaptcha: function() {
+
+      var form, forms, x, status, id, success, error, holder, recaptchaError, siteKey, submit;
+
+      // setup [respond-form]
+      forms = document.querySelectorAll('[respond-form]');
+
+      // setup form
+      for(x=0; x<forms.length; x++) {
+
+        // add submit
+        forms[x].addEventListener('submit', respond.form.submitForm);
+
+        // determine if recaptcha is setup
+        holder = forms[x].querySelector('.g-recaptcha-holder');
+
+        if(holder != null) {
+
+          submit = forms[x].querySelector('[type=submit]');
+
+          if(submit != null) {
+            submit.setAttribute('disabled', 'disabled');
+          }
+
+          (function(form){
+            siteKey = holder.getAttribute('data-sitekey');
+
+            var holderId = grecaptcha.render(holder,{
+              'sitekey': siteKey,
+              'badge' : 'inline', // possible values: bottomright, bottomleft, inline
+              'callback' : function (recaptchaToken) {
+                submit = form.querySelector('[type=submit]');
+
+                if(submit != null) {
+                  submit.removeAttribute('disabled');
+                }
+              }
+            });
+
+            form.onsubmit = function (evt){
+              //evt.preventDefault();
+              //grecaptcha.execute(holderId);
+            };
+          })(forms[x]);
+
+        }
+
+      }
 
   	}
 
