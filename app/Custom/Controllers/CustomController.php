4a3204a15b4ff62e922555065637c0d0ddf529e9
madoublet <matt@matthewsmith.com>
2018-05-06 10:37:48 -0400

    add example custom API extensions

--- app/Custom/Controllers/CustomController.php
+++ app/Custom/Controllers/CustomController.php
@@ -12,13 +12,53 @@ class CustomController
 {
 
   /**
-  * Retrieve the user for the given ID.
-  *
-  * @return Response
-  */
-  public static function test()
+    * Shows how to build an unauthenticated controller in response to a route
+    *
+    * @return Response
+    */
+  public static function example()
   {
-    return '[Respond] Custom API works!';
+    return response('Ok (route works!)', 200);
+  }
+
+  /**
+    * Shows how to build an authenticated controller in response to a route
+    *
+    * @return Response
+    */
+  public static function authExample($request, $param)
+  {
+
+    // get authenticated user's email and the site ID
+    $email = $request->input('auth-email');
+    $siteId = $request->input('auth-id');
+
+    return response('Ok (route works! param='.$param.')', 200);
+  }
+
+  /**
+    * Shows how to build an authenticated controller with a URL parameter in response to a route
+    *
+    * @return Response
+    */
+  public static function authExampleWithParameter($request, $param)
+  {
+
+    // get authenticated user's email and the site ID
+    $email = $request->input('auth-email');
+    $siteId = $request->input('auth-id');
+
+    return response('Ok (route works!)', 200);
+  }
+
+  /**
+    * Shows how to build an authenticated controller with a URL parameter in response to a route
+    *
+    * @return Response
+    */
+  public static function corsExampleWithParameter($request, $param)
+  {
+    return response('Ok (route works! param='.$param.')', 200);
   }
 
 
--- app/Custom/routes.example.php
+++ app/Custom/routes.example.php
@@ -1,9 +1,23 @@
 <?php
+
 /*
- *  Routes specific to Respond PRO
+ *  Sample routes
  */
-// test pro route
-$app->get('/api/custom/example',
+
+// Unauthenticated route
+$app->get('/api/custom/example1',
   function () { return App\Custom\Controllers\CustomController::example(); });
+
+// Authenticated (requires app login) route
+$app->get('/api/custom/example2', ['middleware' => 'jwtauth',
+  function () { return App\Custom\Controllers\CustomController::authExample(app('request')); } ]);
+
+// Authenticated (requires app login) route with parameter
+$app->get('/api/custom/example3/{param}', ['middleware' => 'jwtauth',
+  function ($param) { return App\Custom\Controllers\CustomController::authExampleWithParameter(app('request'), $param); } ]);
+
+// Open route to access server from external website
+$app->get('/api/custom/example4/{param}', ['middleware' => 'cors',
+  function ($param) { return App\Custom\Controllers\CustomController::corsExampleWithParameter(app('request'), $param); } ]);
 
   ?>
